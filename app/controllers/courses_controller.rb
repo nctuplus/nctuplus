f3f70488c23e36723f1be431e7e5612f7e5d090a
u9510606 <u9510606@gmail.com>
2014-07-28 22:34:03 +0800

    google chart gem

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -15,6 +15,7 @@ class CoursesController < ApplicationController
 		Rails.logger.debug "[debug] "+(params[:ct_id].presence|| "nil")
 		ct = CourseTeachership.find(params[:ct_id])
 		@ct_id = ct.id
+
 =begin		
 		@name = Semester.find(ct.course_details.first.semester_id).name
 		if @name.include? "上"
@@ -316,6 +317,26 @@ class CoursesController < ApplicationController
 		# course_teacherships.where(:course_id=>params[:id]).course_teacher_ratings
 		@sems=@course.semesters
 	#@teachers=Teacher.where(:course_id=>@course.id)
+	
+	data_table = GoogleVisualr::DataTable.new
+		# Add Column Headers
+	data_table.new_column('string', 'Year' )
+	data_table.new_column('number', 'Sales')
+	data_table.new_column('number', 'Expenses')
+
+		
+# Add Rows and Values
+	data_table.add_rows([
+    ['2004', 1000, 400],
+    ['2005', 1170, 460],
+    ['2006', 660, 1120],
+    ['2007', 1030, 540]
+	])
+	
+	option = { width: 400, height: 240, title: 'Company Performance' }
+	@chart = GoogleVisualr::Interactive::AreaChart.new(data_table, option)
+	
+	
   end
 	
 	def simulation
