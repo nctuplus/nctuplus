e1359b17f49fbc3575cdcf867066b0da74a7e22d
u9510606 <u9510606@gmail.com>
2014-08-01 09:37:30 +0800

    fix bug

--- app/models/course_teachership.rb
+++ app/models/course_teachership.rb
@@ -2,13 +2,13 @@ class CourseTeachership < ActiveRecord::Base
   has_many :course_details, :dependent=> :destroy
 	has_many :course_teacher_ratings, :dependent=> :destroy
 	has_many :discusses
-<<<<<<< HEAD
+
   has_one :course_content_head
   has_many :course_content_lists
-=======
+
 	has_many :file_infos
-  has_one :course_teacher_page_content
->>>>>>> 21816fae2072db3e3091391f91759068ecd2dfff
+  #has_one :course_teacher_page_content
+
 	
   belongs_to :course
   belongs_to :teacher
--- app/models/raider_content_list.rb
+++
@@ -1,6 +0,0 @@
-class RaiderContentList < ActiveRecord::Base
-	belongs_to :course_teacher_page_content
-	belongs_to :user
-	has_many :content_list_ranks, :dependent => :destroy
-	
-end
