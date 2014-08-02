d3c089653ee30c79ce947a36a2cc504abcffc5d2
tychien <t6847kimo@gmail.com>
2014-08-03 04:08:03 +0800

    fix add semester in simulation

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -414,21 +414,11 @@ class CoursesController < ApplicationController
   end
 	
 	def simulation
-    #@semesters=Semester.all	
-		
-		#@departments=Department.where(:dept_type=>'dept')
-		#@departments_all_select=@departments.map{|d| {"walue"=>d.id, "label"=>d.ch_name}}.to_json
-		#@departments_grad_select=@departments.select{|d|d.degree=='2'}.map{|d| {"walue"=>d.id, "label"=>d.ch_name}}.to_json
-		#@departments_under_grad_select=@departments.select{|d|d.degree=='3'}.map{|d| {"walue"=>d.id, "label"=>d.ch_name}}.to_json
-		#@departments_common_select=@departments.select{|d|d.degree=='0'}.map{|d| {"walue"=>d.id, "label"=>d.ch_name}}.to_json
-		
-		#@degree_select=[{"walue"=>'3', "label"=>"大學部[U]"},{"walue"=>'2', "label"=>"研究所[G]"},{"walue"=>'0', "label"=>"大學部共同課程[C]"}].to_json
-		
-		#@semester_select=Semester.all.select{|s|s.courses.count>0}.map{|s| {"walue"=>s.id, "label"=>s.name}}.to_json
-		@user_sem_ids=current_user.course_simulations.map{|cs|cs.semester_id} 
-		#@user_sem_ids.append(Semester.last.id)
-		
-		
+    
+		@user_sem_ids=current_user.course_simulations.map{|cs|cs.semester_id}
+		if @user_sem_ids.empty?
+			@user_sem_ids=latest_semester.id
+		end
   end
 	
   def new
