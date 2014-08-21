54b2f311b69b8f2edbc6d26f45c5d8efd4665d34
tychien <t6847kimo@gmail.com>
2014-08-21 20:36:30 +0800

    fix image problem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Nctuplus::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
-	config.assets.precompile += %w( courses.js )
+	#config.assets.precompile += %w( courses.js )
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
@@ -77,4 +77,6 @@ Nctuplus::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+	
+	config.logger = Logger.new(config.paths["log"].first, 'daily') 
 end
