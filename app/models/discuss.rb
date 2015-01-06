87b4589c74513750460e32c306df5e69f2fc5a19
tychien <t6847kimo@gmail.com>
2015-01-06 20:55:00 +0800

    fix discuss

--- app/models/discuss.rb
+++ app/models/discuss.rb
@@ -1,5 +1,5 @@
 class Discuss < ActiveRecord::Base
-	belongs_to :course_teachership, counter_cache: true
+	belongs_to :course_teachership#, counter_cache: true
 	belongs_to :user
 	has_many :discuss_likes, :dependent => :destroy
 	has_many :sub_discusses, :dependent => :destroy
