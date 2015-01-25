1cb1473dfb12f95faeed0ee0db41257c5ae11f03
tychien <t6847kimo@gmail.com>
2015-01-26 02:12:01 +0800

    fix course js

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,1 @@
-Rails.application.config.assets.precompile +=['course_content.js','coursemap_manage.js','tmpl.min.js','bootstrap-treeview.js']
+Rails.application.config.assets.precompile +=['course_content.js','coursemap_manage.js','tmpl.min.js','bootstrap-treeview.js','courses/main.js','courses/course_chart.js']
