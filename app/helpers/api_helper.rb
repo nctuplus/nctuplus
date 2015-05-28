672f1a10107f064e38d6cc96d7d95b0d797b9592
t6847kimo <t6847kimo@gmail.com>
2015-05-28 23:34:12 +0800

    fix course rake

--- app/helpers/api_helper.rb
+++ app/helpers/api_helper.rb
@@ -1,36 +1,6 @@
 module ApiHelper
 	
-	def update_teacher_list
-		COURSE_LOGGER.info "Updating Teacher..."
-		teachers=E3Service.get_teacher_list	
-		tids=teachers.map{|t|t["TeacherId"]}
-		@deleted=Teacher.update_all({:is_deleted=>true},["real_id NOT IN (?)",tids])
-		COURSE_LOGGER.info "[Teacher] Total #{@deleted}  Deleted."
-		all_now=Teacher.all.map{|t|{"TeacherId"=>t.real_id, "Name"=>t.name}}
-		@new=teachers - all_now
-		@new.each do |t|
-			@teacher=Teacher.create(
-				:real_id=>t["TeacherId"],
-				:name=>t["Name"],
-				:is_deleted=>false
-			)
-			COURSE_LOGGER.info "[Teacher] - #{@teacher.name} Created."
-		end
-		COURSE_LOGGER.info "[Teacher] Total:#{@new.length} Created."
-	end
-	
-	def update_department_list
-		COURSE_LOGGER.info "Updating Department..."
-		new_depts=E3Service.get_department_list
-		Department.all.each do |dept|
-			new_depts=new_depts.reject{|new_dept|new_dept["degree"].to_i==dept.degree&&new_dept["dep_id"]==dept.dep_id}
-		end
-		new_depts.each do |dept|
-			@dept=Department.create_from_e3(dept)
-			COURSE_LOGGER.info "[Department] - #{@dept.ch_name} Created."
-		end
-		COURSE_LOGGER.info "[Department] Total:#{new_depts.length} Created."
-	end
+
 	
 	
 	
@@ -47,60 +17,7 @@ module ApiHelper
 		end
 	end
 	
-=begin		
-	def save_cd(data,ct_id,sem_id)
-		@cd=CourseDetail.where(:temp_cos_id=>data["cos_id"], :semester_id=>sem_id).take
-		if @cd.nil?
-			@cd=CourseDetail.new 
-			ret="Create"
-		else
-			ret="Update"
-		end
-		@cd.course_teachership_id=ct_id
-		@cd.department_id=get_deptid(data["degree"].to_i,data["dep_id"])
-		@cd.semester_id=sem_id
-		@cd.grade=data["grade"]
-		costime=data['cos_time'].split(',')
-		@cd.time=""
-		@cd.room=""
-		costime.each do |t|
-			_time=t.partition('-')
-			@cd.time<<_time[0]
-			@cd.room<<_time[2]
-		end
-		@cd.cos_type=data["cos_type"]
-		@cd.temp_cos_id=data["cos_id"]
-		@cd.memo=data["memo"]
-		@cd.students_limit=data["num_limit"]
-		@cd.reg_num=data["reg_num"]
-		@cd.brief=data["brief"]
-		@cd.save!
-		return ret
-	end
-	
 
-
-	def get_cid_by_real_id(data)
-		course=Course.where(:real_id=>data["cos_code"]).take
-		if course.nil?
-			course=Course.new
-			course.real_id=data["cos_code"]
-			course.ch_name=data["cos_cname"]
-			course.eng_name=data["cos_ename"]
-			course.credit=data["cos_credit"].to_i
-			#course.department_id=get_deptid(data["degree"].to_i,data["dep_id"])
-			course.save!
-		end
-		return course.id
-	end
-	
-	
-	def get_deptid(degree,dep_id)
-		return 0 if dep_id==""
-		dept=Department.where(:degree=>degree, :dep_id=>dep_id).take
-		return dept.nil? ? 0 : dept.id 
-	end
-=end	
 
 	def parse_scores(score)
 		agree=[]
