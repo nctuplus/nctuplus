098e3d32ac7cc8aaf15f91b3808c61a9433dae79
t6847kimo <t6847kimo@gmail.com>
2015-06-08 23:26:12 +0800

    books new ver 0.5

--- app/models/book_trade_info.rb
+++ app/models/book_trade_info.rb
@@ -1,6 +1,7 @@
 class BookTradeInfo < ActiveRecord::Base
   belongs_to :user
-	
+	belongs_to :book
+	has_many :course_teacherships, :through=>:book
 	validates_numericality_of :price, :only_integer => true
 	validates_length_of :desc, :maximum => 64
 	validates_format_of :url, :with =>  /(^$)|(^((http|https):\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix
