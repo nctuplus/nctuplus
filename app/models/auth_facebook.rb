9e725d219e8f994c9b1bed7ef0afba4b15fb66c7
cwlo <u9510606@gmail.com>
2015-06-12 01:11:56 +0800

    user binding test 1

--- app/models/auth_facebook.rb
+++ app/models/auth_facebook.rb
@@ -19,16 +19,17 @@ class AuthFacebook < ActiveRecord::Base
       f.email = auth.info.email
       
       if f.user_id  # has auth fb
-        if current_user.try(:hasE3?) and not current_user.try(:hasFB?)
-					f.user.merge_child_to_newuser(current_user) # merge to e3 user 
+        if current_user.try(:hasE3?) and not current_user.try(:hasFB?) # bind fb user to e3 user
+					old_user = f.user
+					f.user.merge_child_to_newuser(current_user) # merge to e3 user (main e3)
 					f.user_id = current_user.id  
+					old_user.destroy
 				end
       else # new auth fb user 
-				if current_user
-          f.user_id = current_user.id # binding
-        else# 
+				if current_user # new auth fb and login as e3
+          f.user_id = current_user.id # bind new fb to e3 user
+        else
           user = User.create_from_auth({
-            :provider=>"facebook",
             :name=>auth.info.name,
             :email=>"#{Devise.friendly_token[0,8]}@please.change.me",#auth.info.email,
             :password=>Devise.friendly_token[0,20]
