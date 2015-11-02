b84aabb6e5af89b04018f8e064474d8ca1ee754b
t6847kimo <t6847kimo@gmail.com>
2015-11-02 21:29:53 +0800

    fix js bug

--- app/assets/javascripts/discuss.js
+++ app/assets/javascripts/discuss.js
@@ -2,22 +2,11 @@
  *
  * Copyright (C) 2015 NCTU+
  *
- * For discuss controller (in course/show)
+ * Current now using
  *
  * Modified at 2015/4/24
  */
  
-
-function show_sub_form(id,enabled){
-	if(enabled){
-		$(".sub-form").html("");
-		$("#sub_form_"+id).html('<%=j render "sub_discuss_form" %>');
-		$("#reply_discuss_id").val(id);
-		$("#sub_content").focus();
-	}else{
-		alert("請先登入!");
-	}
-}
 function like(_like,type,id,obj){
 
 	$.ajax({
