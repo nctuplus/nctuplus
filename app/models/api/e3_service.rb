03315d6a8728836b4034bdba2eb4d94b2a2f7720
t6847kimo <t6847kimo@gmail.com>
2016-01-09 12:50:04 +0800

    refactor course import

--- app/models/api/e3_service.rb
+++ app/models/api/e3_service.rb
@@ -1,43 +1,46 @@
 class E3Service 
 
 
-def self.login(username, password)
-
-		return if not username and not password
-require 'open-uri'
-require 'net/http'
-	sendE3={:key=>Nctuplus::Application.config.secret_key_base ,:id=>username, :pwd=>password}				
-	res = Curl.post("#{E3::URL}/Authentication",sendE3).body_str.to_s	
-	if res=='"OK"'
-		return {:auth=>true, :uid=>username}
-	else
-		return {:auth=>false}	
+	def self.login(username, password)
+		return if username.blank? and password.blank?
+		require 'open-uri'
+		require 'net/http'
+		sendE3={:key=>Nctuplus::Application.config.secret_key_base ,:id=>username, :pwd=>password}				
+		res = self._getRawData("Authentication",sendE3)	
+		if res=='"OK"'
+			return {:auth=>true, :uid=>username}
+		else
+			return {:auth=>false}	
 		end
-end
-
-def self.get_course(sem)
-	sendE3={:acy=>sem.year, :sem=>sem.half} 
-	http=Curl.post("#{E3::URL}/CourseList",sendE3)
-
-	return JSON.parse(http.body_str.force_encoding("UTF-8"))
-	
-end
-
-def self.get_teacher_list
-	http=Curl.get("#{E3::URL}/TeacherList",{})
-	return JSON.parse(http.body_str.force_encoding("UTF-8"))
-end
-
-def self.get_department_list
-	http=Curl.post("#{E3::URL}/DepartmentList",{})
-	
-	begin 
-		x=JSON.parse(http.body_str.force_encoding("UTF-8"))
-	rescue JSON::ParserError => e
-		x=nil
-		InformMailer.course_import("Error occured on E3!").deliver
 	end
-	return x
-end	
+
+	def self.getCourse(sem)
+		return _getJsonData("CourseList",{:acy=>sem.year, :sem=>sem.half})
+	end
+
+	def self.getTeacherList
+		return _getJsonData("TeacherList",{})
+	end
+
+	def self.getDepartmentList
+		return _getJsonData("DepartmentList",{})
+	end
+	
+private
+
+	def self._getRawData(url,parameter)
+		return Curl.post("#{E3::URL}/#{url}",parameter).body_str.force_encoding("UTF-8")
+	end
+	
+	def self._getJsonData(url,parameter)
+		begin 
+			return JSON.parse(_getRawData(url,parameter))
+		rescue JSON::ParserError => e
+			InformMailer.course_import("Error Occurred on E3 API!<br> Source:#{url}").deliver
+			raise "E3 API Error"
+		end
+		#return x
+	end
+	
 	
 end 
