b4fa67fbdd160313d0f34350af0a951a7228761b
Terry Liu <hsnuskywind@gmail.com>
2016-02-14 00:46:37 +0800

    database migration (watch events)

--- db/schema.rb
+++ db/schema.rb
@@ -338,25 +338,35 @@ ActiveRecord::Schema.define(version: 20160213064221) do
   add_index "discusses", ["course_teachership_id"], name: "index_discusses_on_course_teachership_id", using: :btree
   add_index "discusses", ["user_id"], name: "index_discusses_on_user_id", using: :btree
 
+  create_table "event1s", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.boolean  "is_public"
+    t.integer  "capacity"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "status"
+  end
+
   create_table "events", force: true do |t|
     t.string   "event_type"
     t.string   "title",                                                 null: false
     t.string   "organization"
-    t.string   "location",                                              null: false
+    t.string   "location"
     t.string   "lat_long"
     t.string   "url",                limit: 2083
-    t.text     "content",            limit: 2147483647,                 null: false
+    t.text     "content",            limit: 2147483647
     t.datetime "begin_time",                                            null: false
     t.datetime "end_time",                                              null: false
     t.integer  "user_id",                                               null: false
     t.integer  "view_times",                            default: 0,     null: false
-    t.boolean  "banner",                                default: false
     t.string   "cover_file_name"
     t.string   "cover_content_type"
     t.integer  "cover_file_size"
     t.datetime "cover_updated_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "banner",                                default: false, null: false
   end
 
   add_index "events", ["user_id"], name: "index_events_on_user_id", using: :btree
