d2eb4dbbd52aac6dca9be6d0686da0117f686568
Yukai Huang <yukaihuangtw@gmail.com>
2016-08-05 08:01:32 +0800

    delete duplicate config folder

--- config/config/initializers/paperclip.rb
+++
@@ -1,7 +0,0 @@
-Paperclip::interpolates :ct_id do |attachment, style|
-  attachment.instance.course_teachership_id 
-end
-
-Paperclip::interpolates :user_id do |attachment, style|
-  attachment.instance.user_id
-end
--- config/config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Nctuplus::Application.config.session_store :cache_store, key: '_nctuplus_session'
