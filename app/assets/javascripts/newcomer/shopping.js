fbe4884d26569d3308763d12112dcd74d9460e3b
Kamigami55 <samkami@icloud.com>
2016-08-07 21:54:47 +0800

    Add newcomer javascripts

---
+++ app/assets/javascripts/newcomer/shopping.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    $('#list').click(function(event){event.preventDefault();$('#products .item').addClass('list-group-item');});
+    $('#grid').click(function(event){event.preventDefault();$('#products .item').removeClass('list-group-item');$('#products .item').addClass('grid-group-item');});
+});
