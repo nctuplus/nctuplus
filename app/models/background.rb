df8bff76bfe32a588f2c279ad94b6e8e45db8b10
t6847kimo <t6847kimo@gmail.com>
2016-11-04 00:26:16 +0800

    move background storage path

--- app/models/background.rb
+++ app/models/background.rb
@@ -1,5 +1,9 @@
 class Background < ActiveRecord::Base
   validates_presence_of :image
-  has_attached_file :image, styles: { medium: "300x300#", thumb: "100x100#" }
+  has_attached_file :image,
+		:default_style => :medium,
+		:url           => '/backgrounds/:id'
+		
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+	validates_with AttachmentSizeValidator, :attributes => :image, :less_than => 1.megabytes
 end
--- app/models/event_image.rb
+++ app/models/event_image.rb
@@ -1,10 +1,8 @@
 class EventImage < ActiveRecord::Base
 	has_attached_file :img,
-		:default_url => "/images/:style/missing.png",
 		:default_style => :medium,
-		#:use_timestamp => false,
-		:url => '/event_images/:id/:filename'#,
-		#:path => '/event/show_image/:id'
+		:default_url   => "/images/:style/missing.png",
+		:url           => '/event_images/:id/:filename'
 	
   validates_attachment_content_type :img, :content_type => /\Aimage\/.*\Z/
 	validates_with AttachmentSizeValidator, :attributes => :img, :less_than => 4.megabytes
