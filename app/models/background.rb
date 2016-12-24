5ff287f07c06cfe5f009bb5ba9a305ad2c26dcda
t6847kimo <t6847kimo@gmail.com>
2016-12-24 23:02:23 +0800

    fix background delete issue_3

--- app/models/background.rb
+++ app/models/background.rb
@@ -2,7 +2,7 @@ class Background < ActiveRecord::Base
   validates_presence_of :image
   has_attached_file :image,
 		:default_style => :medium,
-		:url           => '/backgrounds/:id'
+		:url           => '/backgrounds/:filename'
 		
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 	validates_with AttachmentSizeValidator, :attributes => :image, :less_than => 1.megabytes
