4a0bcd3daa9c33a5a1cb2479547967ba7267a488
yuan310979 <chc71340@gmail.com>
2017-02-20 00:03:56 +0800

    Fix the import of the courses will cause the deletion of simulation(No FORMAT adjestment!!!) (#36)
    * Fix the import of the courses will cause the deletion of simulation
    * modify the imported rules on deleting all the current_user.nomal_scores
    * seek all around the imported course to check if there have courses
    which semester_id is equal to Semesrer::LAST.id
    => if true then delete all the current_user.normal_scores
    => if not then delete all the current_user.normal_scores except for the
    current semester course
    
    * Change the checking method from recursive seeking to finding the last import course

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -30,7 +30,13 @@ class ScoresController < ApplicationController
 			end
 
 			if normal.length > 0				
-				current_user.normal_scores.destroy_all
+			        current_user.normal_scores(:course_detail).each do |course|
+                                  if course.semester_id != Semester::LAST.id
+                                    course.destroy
+                                  elsif course.score != "修習中"
+                                    course.destroy
+                                  end
+                                end
 				current_user.agreed_scores.destroy_all
 			else
 				alertmesg("error",'',"匯入失敗!")
@@ -60,6 +66,19 @@ class ScoresController < ApplicationController
 			@no_pass 				= 0	#沒過
 			@fail_added 		= 0	#匯入失敗
 			@now_taking 		= 0 #正在修
+
+                        # Prevent the import of course will lead to the deletion of simulation
+                        # Check the last import course is the last semester or not
+                        # ex: LAST:105下 CURRENT:105上
+                        # => 模擬排課是放105上的內容，而成績只會匯入到104下的部分，所以會保留105上的模擬排課
+                        # ex: LAST=CURRENT=105下
+                        # => 匯入的成績包含105下，代表模擬排課的內容無須再保留，可刪除依最新教務系統上的選課為主
+                        sem = normal.last['sem']
+                        sem=Semester.where(:year=>sem[0..sem.length-2].to_i, :half=>sem[sem.length-1].to_i).take
+                        if sem.id = Semester::LAST.id
+                          current_user.normal_scores.destroy_all
+                        end
+
 			normal.each do |n|
 				if n['score'] == "通過" || n['score'].to_i>=current_user.pass_score
 					@pass+=1
