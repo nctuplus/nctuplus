aac680986f052a5300394cce870b93fafe695e20
ss8651twtw <ss8651twtw@gmail.com>
2017-03-09 00:27:56 +0800

    remove members table

--- db/schema.rb
+++ db/schema.rb
@@ -428,23 +428,6 @@ ActiveRecord::Schema.define(version: 20170131153409) do
     t.datetime "updated_at"
   end
 
-  create_table "members", force: true do |t|
-    t.string   "name"
-    t.string   "nickname"
-    t.string   "job"
-    t.string   "department"
-    t.string   "motivation"
-    t.string   "others"
-    t.string   "fb"
-    t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "member_image_file_name"
-    t.string   "member_image_content_type"
-    t.integer  "member_image_file_size"
-    t.datetime "member_image_updated_at"
-  end
-
   create_table "normal_scores", force: true do |t|
     t.integer "user_id",          default: 0,  null: false
     t.integer "course_detail_id", default: 0,  null: false
