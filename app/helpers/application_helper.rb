1506c1829b40bdccd494d80e70f64e24546d5f92
Yuan-Yao Sung <sungyuanyao@gmail.com>
2017-06-20 14:40:21 +0800

    Add nctuplus_developer to select user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,21 @@
 module ApplicationHelper
-  
+
   def user_avatar(name, url, h, w)
     return "<img alt='#{name}' height='#{h}' width='#{w}' src='#{url}'>".html_safe
   end
 
-	def loading_img
-		html="<p class='text-center'>#{fa_icon("refresh spin 2x")}</p>"
-		return html.html_safe
-	end
-		def numeric?(string)
-	  # `!!` converts parsed number to `true`
-	  !!Kernel.Float(string) 
-	rescue TypeError, ArgumentError
-	  false
-	end
+  def loading_img
+    html="<p class='text-center'>#{fa_icon("refresh spin 2x")}</p>"
+    return html.html_safe
+  end
+  def numeric?(string)
+    # `!!` converts parsed number to `true`
+    !!Kernel.Float(string) 
+  rescue TypeError, ArgumentError
+    false
+  end
+
+  def nctuplus_developer
+    current_user && (current_user.role == 0 || current_user.role == 3)
+  end
 end
