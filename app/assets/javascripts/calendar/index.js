fb12c426171bbe5e9b2fbbd0ed199c26c3d370c7
Ho-Sheng <chao.hs1031@gmail.com>
2017-07-14 11:40:00 +0800

    Convert indentation to Spaces for some files.

--- app/assets/javascripts/calendar/index.js
+++ app/assets/javascripts/calendar/index.js
@@ -21,91 +21,91 @@ var ThisMonLastDate = LastDate.getDate();
 
 
 function refreshSelctedMonth(){
-	
-	firstDate = new Date(selectedYear, selectedMonth, 1);
-	firstDateDay = firstDate.getDay();
+  
+  firstDate = new Date(selectedYear, selectedMonth, 1);
+  firstDateDay = firstDate.getDay();
 
-	PreMon = new Date(selectedYear, selectedMonth, 0);
-	PreMonLastDate = PreMon.getDate();
+  PreMon = new Date(selectedYear, selectedMonth, 0);
+  PreMonLastDate = PreMon.getDate();
 
-	if(firstDateDay == 0)
-		firstSundayDate = 1;
-	else
-		firstSundayDate = PreMonLastDate - firstDateDay+1 ;
+  if(firstDateDay == 0)
+    firstSundayDate = 1;
+  else
+    firstSundayDate = PreMonLastDate - firstDateDay+1 ;
 
-	LastDate = new Date(selectedYear, selectedMonth+1, 0);
-	ThisMonLastDate = LastDate.getDate();
+  LastDate = new Date(selectedYear, selectedMonth+1, 0);
+  ThisMonLastDate = LastDate.getDate();
 
 }
 
 function refreshDate() {
-	var i;
-	var tempDate = firstSundayDate;
-	var monOffset = -1;
-	if(tempDate == 1) monOffset = 0;
+  var i;
+  var tempDate = firstSundayDate;
+  var monOffset = -1;
+  if(tempDate == 1) monOffset = 0;
 
-	for(i=0; i<42; i++){
+  for(i=0; i<42; i++){
 
-		if(monOffset != 0)
-			$(".date-box").eq(i).addClass("not-this-month");
-		else{
-			$(".date-box").eq(i).removeClass("not-this-month");
-			if(tempDate == cdate && selectedMonth == cmonth){
-				$(".date-box .date").eq(i).addClass("date-today");
-				$(".date-box").eq(i).addClass("box-today");
-			}else{
-				$(".date-box .date").eq(i).removeClass("date-today");
-				$(".date-box").eq(i).removeClass("box-today");
-			}
-		}
-		$(".date").eq(i).text(tempDate);
+    if(monOffset != 0)
+      $(".date-box").eq(i).addClass("not-this-month");
+    else{
+      $(".date-box").eq(i).removeClass("not-this-month");
+      if(tempDate == cdate && selectedMonth == cmonth){
+        $(".date-box .date").eq(i).addClass("date-today");
+        $(".date-box").eq(i).addClass("box-today");
+      }else{
+        $(".date-box .date").eq(i).removeClass("date-today");
+        $(".date-box").eq(i).removeClass("box-today");
+      }
+    }
+    $(".date").eq(i).text(tempDate);
 
-		if(monOffset == -1 && tempDate == PreMonLastDate){
-			tempDate = 1;
-			monOffset++;
-		}else if(monOffset == 0 && tempDate == ThisMonLastDate){
-			tempDate = 1;
-			monOffset++;
-		}else
-			tempDate++;
+    if(monOffset == -1 && tempDate == PreMonLastDate){
+      tempDate = 1;
+      monOffset++;
+    }else if(monOffset == 0 && tempDate == ThisMonLastDate){
+      tempDate = 1;
+      monOffset++;
+    }else
+      tempDate++;
 
 
 
-	}
+  }
 }
 
 function nxtMonth() {
-	$("#cal-date").hide().fadeIn(300);
+  $("#cal-date").hide().fadeIn(300);
 
-	if(selectedMonth == 11){
-		selectedMonth = 0;
-		selectedYear++;
-	}else	selectedMonth++;
-	refreshSelctedMonth()
-	refreshDate(); 
-	$("h2").eq(0).text( (selectedMonth+1) + "月 " + selectedYear);
+  if(selectedMonth == 11){
+    selectedMonth = 0;
+    selectedYear++;
+  }else selectedMonth++;
+  refreshSelctedMonth()
+  refreshDate(); 
+  $("h2").eq(0).text( (selectedMonth+1) + "月 " + selectedYear);
 }
 
 function preMonth() {
-	$("#cal-date").hide().fadeIn(300);
+  $("#cal-date").hide().fadeIn(300);
 
-	if(selectedMonth == 0){
-		selectedMonth = 11;
-		selectedYear--;
-	}else	selectedMonth--;
-	refreshSelctedMonth()
-	refreshDate(); 
-	$("h2").eq(0).text( (selectedMonth+1) + "月 " + selectedYear);
+  if(selectedMonth == 0){
+    selectedMonth = 11;
+    selectedYear--;
+  }else selectedMonth--;
+  refreshSelctedMonth()
+  refreshDate(); 
+  $("h2").eq(0).text( (selectedMonth+1) + "月 " + selectedYear);
 }
 
 function thisMonth() {
-	$("#cal-date").hide().fadeIn(300);
+  $("#cal-date").hide().fadeIn(300);
 
-	selectedMonth = cmonth;
-	selectedYear = cyear;
-	refreshSelctedMonth()
-	refreshDate(); 
-	$("h2").eq(0).text( (selectedMonth+1) + "月 " + selectedYear);
+  selectedMonth = cmonth;
+  selectedYear = cyear;
+  refreshSelctedMonth()
+  refreshDate(); 
+  $("h2").eq(0).text( (selectedMonth+1) + "月 " + selectedYear);
 }
 
 
@@ -115,74 +115,74 @@ $("#cal-date").hide();
 
 $(document).ready(function(){
 
-	$("#cal-date").hide().fadeIn(500);
+  $("#cal-date").hide().fadeIn(500);
 
-	refreshDate(); 
-	$("h2").eq(0).text( (selectedMonth+1) + "月 " + selectedYear);
-
-
-	$(".tag-cale").hide();
-	$(".tag-info").hide();
-	$("#btn-bell").addClass("selected");
-
-	$("#btn-cale").click(function(){
-		$(".tag-cale").fadeIn();
-		$(".tag-info").fadeOut();
-		$(".tag-bell").fadeOut();
-		$("#btn-cale").addClass("selected");
-		$("#btn-info").removeClass("selected");
-		$("#btn-bell").removeClass("selected");
-	});
-
-	$("#btn-info").click(function(){
-		$(".tag-cale").fadeOut();
-		$(".tag-info").fadeIn();
-		$(".tag-bell").fadeOut();
-		$("#btn-cale").removeClass("selected");
-		$("#btn-info").addClass("selected");
-		$("#btn-bell").removeClass("selected");
-	});
-
-	$("#btn-bell").click(function(){
-		$(".tag-cale").fadeOut();
-		$(".tag-info").fadeOut();
-		$(".tag-bell").fadeIn();
-		$("#btn-cale").removeClass("selected");
-		$("#btn-info").removeClass("selected");
-		$("#btn-bell").addClass("selected");
-	});
+  refreshDate(); 
+  $("h2").eq(0).text( (selectedMonth+1) + "月 " + selectedYear);
 
 
-	$("#bar1").click(function(){
+  $(".tag-cale").hide();
+  $(".tag-info").hide();
+  $("#btn-bell").addClass("selected");
+
+  $("#btn-cale").click(function(){
+    $(".tag-cale").fadeIn();
+    $(".tag-info").fadeOut();
+    $(".tag-bell").fadeOut();
+    $("#btn-cale").addClass("selected");
+    $("#btn-info").removeClass("selected");
+    $("#btn-bell").removeClass("selected");
+  });
+
+  $("#btn-info").click(function(){
+    $(".tag-cale").fadeOut();
+    $(".tag-info").fadeIn();
+    $(".tag-bell").fadeOut();
+    $("#btn-cale").removeClass("selected");
+    $("#btn-info").addClass("selected");
+    $("#btn-bell").removeClass("selected");
+  });
+
+  $("#btn-bell").click(function(){
+    $(".tag-cale").fadeOut();
+    $(".tag-info").fadeOut();
+    $(".tag-bell").fadeIn();
+    $("#btn-cale").removeClass("selected");
+    $("#btn-info").removeClass("selected");
+    $("#btn-bell").addClass("selected");
+  });
+
+
+  $("#bar1").click(function(){
         $("#bar1").nextAll().toggle("fast");
     });
 
-	
-	$("#bar2").click(function(){
+  
+  $("#bar2").click(function(){
         $("#bar2").nextAll().toggle("fast");
     });
-	
-	$("#bar3").click(function(){
+  
+  $("#bar3").click(function(){
         $("#bar3").nextAll().toggle("fast");
     });
-	
-	$("#bar4").click(function(){
+  
+  $("#bar4").click(function(){
         $("#bar4").nextAll().toggle("fast");
     });
 
-	$("#bar5").click(function(){
+  $("#bar5").click(function(){
         $("#bar5").nextAll().toggle("fast");
     });
 
 /*
-	$('.content').popover({
-				content: $(".content").html(),
-				placement:"auto",
-				trigger : "hover",
-			});
+  $('.content').popover({
+        content: $(".content").html(),
+        placement:"auto",
+        trigger : "hover",
+      });
 
 */
-//	var hey = new Date(1498581000000);
-//	alert(hey);
+//  var hey = new Date(1498581000000);
+//  alert(hey);
 
 });
