72db68ca2a405b468ebb80f5071ec7cde5c79512
osinoyan <chopsticks1515@gmail.com>
2017-09-13 17:30:01 +0800

    add newCommonCourse row at userPage

--- app/assets/javascripts/user_course_stat/checker.js
+++ app/assets/javascripts/user_course_stat/checker.js
@@ -26,6 +26,9 @@ function commonCheck(pass_courses){
 		}
 	}; 
 
+	console.log(pass_courses);
+
+
 	for(var i = 0,course;course=pass_courses[i];i++){ 
 		if(course.name.search("服務學習")!=-1){ 
 			if(course.name.search("一")!=-1){ 
