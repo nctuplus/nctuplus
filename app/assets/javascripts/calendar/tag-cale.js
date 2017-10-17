9c00a2727f10c7f43714e4540980f8197aed31ab
osinoyan <chopsticks1515@gmail.com>
2017-10-18 03:14:23 +0800

    Improve codes for PR

--- app/assets/javascripts/calendar/tag-cale.js
+++ app/assets/javascripts/calendar/tag-cale.js
@@ -7,6 +7,7 @@
 // Js import directive for RoR:
 //= require calendar/calendar.js
 //= require calendar/event-loader.js
+//= require calendar/add-event.js
 
 
 var tagCale = {
@@ -57,75 +58,10 @@ var tagCale = {
 
 
 
-
-  //傳入event物件繪製出來
-  addEvent: function(event) {
-    var title;
-    var eventTime;
-    var content;
-
-    var yy = event.EventTime.getFullYear(), mn = event.EventTime.getMonth()+1, dd = event.EventTime.getDate();
-    var hh = event.EventTime.getHours(), mm = event.EventTime.getMinutes(), ss = event.EventTime.getSeconds();
-
-
-    //判斷event屬性, 以選擇用EventTime或者TimeStart/TimeEnd
-    if(event.MaterialType == 'announcement'){
-      title = event.CourseName;
-      eventTime = ' ' + yy + '-' + mn + '-' + dd;
-      content = event.Title;
-    }else if(event.MaterialType == 'event'){
-      title = event.Title;
-      eventTime = ' ' + yy + '-' + mn + '-' + dd + ' ' + (hh<10?'0':'')+hh + ':' + (mm<10?'0':'')+mm + ':' + (ss<10?'0':'')+ss ;
-      content = '';
-    }else if(event.MaterialType == 'assignment'){
-      title = event.CourseName;
-      hh = event.TimeEnd.getHours(), mm = event.TimeEnd.getMinutes(), ss = event.TimeEnd.getSeconds();
-      yy = event.TimeEnd.getFullYear(), mn = event.TimeEnd.getMonth()+1, dd = event.TimeEnd.getDate();
-      eventTime = '  Deadline  ' + yy + '-' + mn + '-' + dd + '   ' + hh + ':' + mm + ':' + ss;
-      content = event.Title;
-    }else{
-      return false;
-    }
-
-    var e = $(`<div class="thing-cale"><div class="content"><b>${title}</b><br>
-      <span class="glyphicon glyphicon-time"></span>${eventTime}<br>
-        ${content}</div></div>`).appendTo('.tag-cale');
-    
-    switch (event.MaterialType) {
-      case 'announcement':
-        e.find('.content').addClass('border-darkblue'); break;
-      case 'assignment':
-        e.find('.content').addClass('border-red'); break;
-      case 'event':
-        e.find('.content').addClass('border-yellow'); break;
-    }
-    
-    return true;
-
-  },
-
   //傳入日期抓該日的event
   addEventToDate: function(toDate) {
-
-
-    var Nyear = toDate.getFullYear(), Nmonth = toDate.getMonth(), Nd = toDate.getDate();
-    var hasEvent = false;
-
-    //直接搜遍所有event 找到符合該日期的event則丟入addEvent()
-    var savedEvents = eventLoader.events[Nyear*100+Nmonth+1];
-    for(var i=0; (savedEvents!==undefined && i<savedEvents.length); i++){
-      var tmp = savedEvents[i];
-      var year = tmp.EventTime.getFullYear(), month = tmp.EventTime.getMonth(), d = tmp.EventTime.getDate();
-      if(Nyear == year && Nmonth == month && Nd == d){
-        tagCale.addEvent(tmp);
-        hasEvent = true;
-      }      
-    }
-
-    if(!hasEvent) $(`<div class="thing-cale"><div class="content">(無事件)</div></div>`).appendTo('.tag-cale');
-
-
-  }
+    forAddEvent.addEventToDate(toDate, 1, 'cale');
+  },
 
 
 
--- app/assets/javascripts/calendar/tag-info.js
+++ app/assets/javascripts/calendar/tag-info.js
@@ -7,6 +7,7 @@
 // Js import directive for RoR:
 //= require calendar/calendar.js
 //= require calendar/event-loader.js
+//= require calendar/add-event.js
 
 
 var tagInfo = {
@@ -29,65 +30,10 @@ var tagInfo = {
   },
 
 
-
-  //傳入event物件繪製出來
-  addEvent: function(event) {
-    var title;
-    var eventTime;
-    var content;
-
-    var yy = event.EventTime.getFullYear(), mn = event.EventTime.getMonth()+1, dd = event.EventTime.getDate();
-
-
-    //判斷event屬性, 以選擇用EventTime或者TimeStart/TimeEnd
-    if(event.MaterialType == 'announcement'){
-      title = event.CourseName;
-      eventTime = ' ' + yy + '-' + mn + '-' + dd;
-      content = event.Title;
-    }else return false;
-
-    var e = $(`<div class="thing-info"><div class="content border-darkblue"><b>${title}</b><br>
-      <span class="glyphicon glyphicon-time"></span>${eventTime}<br>
-        ${content}</div></div>`).appendTo('.tag-info');
-    
-    
-    
-    return true;
-
-  },
-
   //傳入日期抓該月和上個月的event
+  //抓該月和上個月的e3公告event
   addEventToDate: function(toDate) {
-
-
-    var Nyear = toDate.getFullYear(), Nmonth = toDate.getMonth(), Nd = toDate.getDate();
-    var hasEvent = false;
-
-    //直接搜遍所有該月event
-    var savedEvents = eventLoader.events[Nyear*100+Nmonth+1];
-    //用時間排序(新到舊)
-    savedEvents.sort( function(a, b){return b.EventTime-a.EventTime} );
-    for(var i=0; (savedEvents!==undefined && i<savedEvents.length); i++){
-      //全部加進去
-      var tmp = savedEvents[i];
-      tagInfo.addEvent(tmp);
-      hasEvent = true;
-    }
-
-    //直接搜遍所有上個月event
-    savedEvents = eventLoader.events[Nyear*100+Nmonth];
-    //用時間排序(新到舊)
-    savedEvents.sort( function(a, b){return b.EventTime-a.EventTime} );
-    for(var i=0; (savedEvents!==undefined && i<savedEvents.length); i++){
-      //全部加進去
-      var tmp = savedEvents[i];
-      tagInfo.addEvent(tmp);
-      hasEvent = true;
-    }
-
-    if(!hasEvent) $(`<div class="thing-info"><div class="content">(近期並無公告)</div></div>`).appendTo('.tag-info');
-
-
+    forAddEvent.addEventToDate(toDate, 1, 'info');
   }
 
 
