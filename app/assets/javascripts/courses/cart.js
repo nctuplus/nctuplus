3d71f05f43a13474a7ac88086c4d87d0575ff5a1
Ho-Sheng <chao.hs1031@gmail.com>
2017-12-27 08:43:24 +0800

    Remove add course to cart button on navBar.

--- app/assets/javascripts/courses/cart.js
+++ app/assets/javascripts/courses/cart.js
@@ -23,15 +23,4 @@ function add_to_cart($this_but, id,type){
             $this_but.parent().parent().parent().remove();
 
 }
-function showCart(){
-	$.get("/courses/show_cart",
-		{
-			view_type:"session",
-			use_type:"delete",
-			add_to_cart:"0"
-		},
-		function(data){
-			showGlobalModal(1,"收藏課程",data,function(){$("#global-modal").find(".modal-body").html("");});
-		}
-	);
-}
+
